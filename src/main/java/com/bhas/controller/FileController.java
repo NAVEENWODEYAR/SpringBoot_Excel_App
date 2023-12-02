1559281c1184c6ac84c6fc883aca6e4237079f42
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-02 20:48:04 +0530

    test endPoint

--- src/main/java/com/bhas/controller/FileController.java
+++ src/main/java/com/bhas/controller/FileController.java
@@ -1,9 +1,18 @@
 package com.bhas.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FileController 
 {
 
+	@GetMapping
+	@ResponseStatus(code = HttpStatus.BAD_GATEWAY)
+	public String greetMsg()
+	{
+		return "Welcome to the Excel_File Application,";
+	}
 }
