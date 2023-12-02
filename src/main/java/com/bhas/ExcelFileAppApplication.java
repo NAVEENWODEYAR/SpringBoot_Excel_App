1274925069264afcc00fc2839f914d5949ccb96b
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-02 20:02:34 +0530

    port configured

--- src/main/java/com/bhas/ExcelFileAppApplication.java
+++ src/main/java/com/bhas/ExcelFileAppApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExcelFileAppApplication {
+public class ExcelFileAppApplication
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(ExcelFileAppApplication.class, args);
+		System.out.println("\n Application started");
 	}
 
 }
