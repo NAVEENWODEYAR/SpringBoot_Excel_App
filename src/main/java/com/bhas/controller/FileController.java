06938aa5b5243f6175f4de8752a5138a0a90c5de
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-03 15:29:56 +0530

    swagger added

--- src/main/java/com/bhas/controller/FileController.java
+++ src/main/java/com/bhas/controller/FileController.java
@@ -5,7 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class FileController 
 {
 
@@ -13,6 +16,7 @@ public class FileController
 	@ResponseStatus(code = HttpStatus.BAD_GATEWAY)
 	public String greetMsg()
 	{
+		log.info("LOGGER OBJECT");
 		return "Welcome to the Excel_File Application,";
 	}
 }
